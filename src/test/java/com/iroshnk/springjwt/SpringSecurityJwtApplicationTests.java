6b2e9db83d0200e26ca90ff9bf5af8ce984202b2
Irosh <iroshnk@gmail.com>
2020-05-11 14:04:05 +0530

    jwt

---
+++ src/test/java/com/iroshnk/springjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iroshnk.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
